22703cdd2ea15d44f21a271d234471f645bfd7b4
balatzar <bataprod@gmail.com>
2016-07-13 09:07:09 +0200

    add form set group name

--- imports/ui/layout/adventurePage/adventurePage.js
+++ imports/ui/layout/adventurePage/adventurePage.js
@@ -1,1 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { $ } from 'meteor/jquery';
+
 import './adventurePage.html';
+
+Template.adventurePage.onRendered(() => {
+  Meteor.call('group.getName', FlowRouter.current().params.id, (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      $('.js-groupName').text(res);
+    }
+  });
+});
--- imports/ui/layout/goPage/goPage.js
+++ imports/ui/layout/goPage/goPage.js
@@ -1,1 +1,33 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
 import './goPage.html';
+
+Template.goPage.onRendered(() => {
+  $('.js-createGroup')
+  .form({
+    fields: {
+      name: {
+        identifier: 'name',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Vous devez indiquer un nom pour votre groupe.',
+          },
+        ],
+      },
+    },
+    onSuccess(event, fields) {
+      event.preventDefault();
+      Meteor.call('group.insert', fields.name, (err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          FlowRouter.go(`/adventure/${res}`);
+        }
+      });
+    },
+  });
+});
